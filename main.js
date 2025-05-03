012d3259c6c7152c57ac66a9950ac39211343262
tako162 <tako162.git@google.com>
2025-05-04 00:35:39 +0900

    1.0.0

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
+const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet1');
+const time = new Date();
+
 function doGet(e) {
   const getData = e.parameter;
   const action = getData.action;
   const roomCode = getData.roomCode;
   const userUsedVersion = getData.userUsedVersion;
+  updateTimestamp();
   switch (action) {
     case "getRoomCode":
-      deleteTriggers();
-      createTrigger();
       return getRoomCode();
     case "setRoomCode":
+      deleteTriggers();
+      createTrigger();
       return setRoomCode(roomCode);
     case "checkVersion":
       return checkVersion(userUsedVersion);
@@ -28,13 +32,18 @@ function getRoomCode() {
   }
 }
 
+const roomCodeRange = findRanges("ルームコード", 0, 1, 1);
+const timeLimitRange = findRanges("有効期限(分)", 0, 1, 1);
+const timeLimitValue = sheet.getRange(timeLimitRange[0]).getValue();
+
 function setRoomCode(roomCode) {
   PropertiesService.getScriptProperties().setProperty("ROOM_CODE", roomCode);
-  return ContentService.createTextOutput().append("コードを共有しました！！\n有効期限は10分です");
+  sheet.getRange(roomCodeRange).setValue(roomCode);
+  return ContentService.createTextOutput().append(`コードを共有しました\n有効期限は${timeLimitValue}分です`);
 }
 
 function createTrigger() {
-  ScriptApp.newTrigger('resetRoomCode').timeBased().after(10 * 60 * 1000).create();
+  ScriptApp.newTrigger('resetRoomCode').timeBased().after(timeLimitValue * 60 * 1000).create();
 }
 
 function deleteTriggers() {
@@ -45,9 +54,12 @@ function deleteTriggers() {
 }
 
 function resetRoomCode() {
+  deleteTriggers();
   PropertiesService.getScriptProperties().setProperty("ROOM_CODE", "Expired");
+  sheet.getRange(roomCodeRange).setValue("Expired");
 }
 
+//TODO バージョン番号を分割してチェック=>アップデートの優先度を表示
 function checkVersion(userUsedVersion) {
   const currentVersion = PropertiesService.getScriptProperties().getProperty("VERSION");
   if (userUsedVersion != currentVersion) {
@@ -58,4 +70,26 @@ function checkVersion(userUsedVersion) {
 function getUpdateURL() {
   const url = PropertiesService.getScriptProperties().getProperty("UPDATE_URL");
   return ContentService.createTextOutput().append(url);
+}
+
+function updateTimestamp() {
+  const timestampRanges = findRanges("タイムスタンプ", 0, 1, 1);
+  sheet.getRange(timestampRanges[0]).setValue(time);
+}
+
+function test_getUpdatePriorityMessage() {
+  let test_CurrentVersion = "1.0.1";
+  let test_UserUsedVersion = "1.0.0";
+
+  let split_CurrentVersion = test_CurrentVersion.split(".");
+  let split_UserUsedVersion = test_UserUsedVersion.split(".");
+  console.log(split_CurrentVersion);
+  console.log(split_UserUsedVersion);
+
+  for (let i = 0; i < split_CurrentVersion.length; i++) {
+    // console.log(split_CurrentVersion[i]);
+    if (split_CurrentVersion[i] !== split_UserUsedVersion[i]) {
+      console.log(i);
+    }
+  }
 }
